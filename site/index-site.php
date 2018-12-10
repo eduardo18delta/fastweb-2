fe549facfa6118a59d6fd7f8982c21d8b929f8cc
Brenno Farias <brennohenriqueskt@gmail.com>
2018-12-10 04:11:50 -0200

    Reformulanto todo conteudo.php

---
+++ site/index-site.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Fast Web</title>
+  </head>
+  <body>
+    <?php
+      include 'menu.php';
+      include 'slide.php';
+      include 'conteudo-site.php';
+      include 'rodape.php';
+    ?>
+  </body>
+</html>
