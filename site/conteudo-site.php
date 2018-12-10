43f5d671c037d3f8c6f1ed5f473249e0c3bd4cce
Brenno Farias <brennohenriqueskt@gmail.com>
2018-12-10 02:35:49 -0200

    Renponsividade do conteudo.php

---
+++ site/conteudo-site.php
@@ -0,0 +1,14 @@
+<?php
+  include 'links.php';
+?>
+<html>
+  <head>
+    <title></title>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <main class="container-fluid">
+
+    </main>
+  </body>
+</html>
--- site/conteudo.php
+++ site/conteudo.php
@@ -1,11 +1,10 @@
-<?php
-  include 'links.php';
-?>
+
 <html>
   <head>
     <title></title>
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="_css/conteudo.css"/>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   </head>
   <body>
@@ -14,126 +13,125 @@
       <section class="d-flex flex-column align-items-center mt-5">
         <h4 class="tituloProds">Produtos em promoção</h4>
         <h6>Aqui tem! Compre antes que acabe!</h6>
-        <div class="d-flex flex-wrap justify-content-center bordaCima">
-
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+        <div class="container-fluid d-flex flex-wrap justify-content-between justify-content-lg-center bordaCima">
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
-            </div>
-          </div>
-
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
 
-          <div class="col-3 m-2 prod">
-            <div class="fotoProd">
-              <img src="_img/home/prod1.jpg" />
-            </div>
-            <div class=" d-flex flex-column align-items-center infoProd">
-              <p>Calabresa</p>
-              <div class="precoProd">
-                <span class="preco">R$ 10,00</span>
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
               </div>
-              <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
             </div>
-          </div>
+
+            <div class="col-lg-3 m-2 prod">
+              <div class="fotoProd">
+                <img src="_img/home/prod1.jpg" />
+              </div>
+              <div class=" d-flex flex-column align-items-center infoProd">
+                <p>Calabresa</p>
+                <div class="precoProd">
+                  <span class="preco">R$ 10,00</span>
+                </div>
+                <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+              </div>
+            </div>
         </div>
-        <input type="submit" class="btn btn-warning align-self-start btnMais" value="Clique aqui e veja mais produtos"/>
+        <input type="submit" class="btn btn-warning align-self-center align-self-lg-start btnMais" value="Clique aqui e veja mais produtos"/>
       </section>
     <!--=========FIM PROMOÇÃO=============-->
     <!--=========MAIS VENDIDOS============-->
@@ -265,3 +263,6 @@
     </main>
   </body>
 </html>
+<?php
+  include 'links.php';
+?>
