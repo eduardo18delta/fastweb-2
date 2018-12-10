ddfb86cdee82fe82ea7b021f60b0e583b1df2295
Brenno Farias <brennohenriqueskt@gmail.com>
2018-12-10 03:12:08 -0200

    Teste responsividade conteudo.php

--- site/conteudo-site.php
+++ site/conteudo-site.php
@@ -6,11 +6,36 @@
     <title></title>
     <meta charset="utf-8"/>
   </head>
+  <style>
+    .borda{
+      border:1px solid #000;
+    }
+    .info-prod{
+      font-size:20pt;
+    }
+    .preco{
+      font-size:25pt;
+    }
+    .btn-comprar{
+      font-size:20pt;
+    }
+  </style>
   <body>
-    <main class="container-fluid">
-      <div class="">
-        <div>
-          <img src="_img/home/prod1.jpg" />
+    <main class="container-fluid borda">
+      <div class="row borda" style="border:1px solid red;">
+        <div class="borda">
+          <div class="borda">
+            <img src="_img/home/prod1.jpg" class=""/>
+          </div>
+          <div class="text-center borda info-prod">
+            <p class="d-inline align-middle">Calabresa</p>
+          </div>
+          <div class="text-center preco borda">
+            <p class="d-inline align-middle text-danger">R$ 10,00</p>
+          </div>
+          <div class="text-center borda">
+            <input type="text" class="btn btn-danger my-2 w-50 btn-comprar" value="Comprar"/>
+          </div>
         </div>
       </div>
       <div class="">
