a6e658678e195aab4e13e57410d9cd55fea3c68d
Eduardo Henrique <eduardo18delta@gmail.com>
2019-04-08 11:53:39 -0300

    Modificando texto

--- view/loginclienteView.php
+++ view/loginclienteView.php
@@ -12,7 +12,7 @@
 	<body>
 		<div class="container">
 			<div class="form-signin" style="background: #42dea4;">
-				<h2 class="text-center">Área restrita</h2>
+				<h2 class="text-center">Login Cliente</h2>
 				<?php
 					if(isset($_SESSION['msg'])){
 						echo $_SESSION['msg'];
@@ -48,6 +48,5 @@
 			</div>
 		</div>			
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
