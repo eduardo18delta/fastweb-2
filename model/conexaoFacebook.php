7ec42706ad8ef26f35464439e5c203766ee03295
Eduardo Henrique <eduardo18delta@gmail.com>
2019-04-08 10:45:38 -0300

    removendo função loginfacebook() e redirecionando para logoutfacebookController()

---
+++ model/conexaoFacebook.php
@@ -0,0 +1,8 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "drefj508";
+	$dbname = "fastweb-2";
+	
+	//Criar a conexao
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
--- view/perfilclienteView.php
+++ view/perfilclienteView.php
@@ -3,7 +3,7 @@
 session_start();
 if(!empty($_SESSION['id'])){
 	echo "Olá ".$_SESSION['nome'].", Bem vindo <br>";
-	echo "<a href='sair.php'>Sair</a>";
+	echo "<a href='../controller/logoutfacebookController.php'>Sair</a>";
 }else{
 	$_SESSION['msg'] = "<div class='alert alert-danger'>Área restrita!</div>";
 	header("Location: loginclienteView.php");	
