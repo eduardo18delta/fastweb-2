d7bc3e21a7114b8ee460b84ac64d48b7a275ecd0
Eduardo Henrique <eduardo18delta@gmail.com>
2019-04-08 20:45:55 -0300

    personalizando o login do cliente

--- view/loginclienteView.php
+++ view/loginclienteView.php
@@ -1,52 +1,52 @@
 <?php include_once '../controller/facebookloginController.php'; ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Fastweb- Login</title>
-		<link href="../assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
-		<link href="../assets/css/login-cadastro-cliente.css" rel="stylesheet">
-	</head>
+<?php include_once '../view/menuView.php'; ?>
+
 	<body>
 		<div class="container">
-			<div class="form-signin" style="background: #42dea4;">
-				<h2 class="text-center">Login Cliente</h2>
-				<?php
-					if(isset($_SESSION['msg'])){
-						echo $_SESSION['msg'];
-						unset($_SESSION['msg']);
-					}
-					if(isset($_SESSION['msgcad'])){
-						echo $_SESSION['msgcad'];
-						unset($_SESSION['msgcad']);
-					}
-				?>
-				<form method="POST" action="valida.php">
-					<!--<label>Usuário</label>-->
-					<input type="text" name="usuario" placeholder="Digite o seu usuário" class="form-control"><br>
+			<div class="row justify-content-center">
+				<div class="col-md-4">
+					<div class="form-signin">
+						<h2 class="text-center">Login Cliente</h2>
+						<?php
+							if(isset($_SESSION['msg']))
+							{
+								echo $_SESSION['msg'];
+								unset($_SESSION['msg']);
+							}
+
+							if(isset($_SESSION['msgcad']))
+							{
+								echo $_SESSION['msgcad'];
+								unset($_SESSION['msgcad']);
+							}
+						?>
+						<form method="POST" action="valida.php">				
+							<div class="form-group">
+								<input type="text" name="usuario" placeholder="Digite o seu usuário" class="form-control">
+							</div>
+									
+							<div class="form-group">									
+								<input type="password" name="senha" placeholder="Digite a sua senha" class="form-control">
+							</div>
 					
-					<!--<label>Senha</label>-->
-					<input type="password" name="senha" placeholder="Digite a sua senha" class="form-control"><br>
+							<div class="form-group">									
+								<input type="submit" name="btnLogin" value="Acessar" class="btn btn-success btn-block">
+							</div>	
 					
-					<input type="submit" name="btnLogin" value="Acessar" class="btn btn-success btn-block">
-					
-					<div class="row text-center" style="margin-top: 20px;"> 
-						<h4>Você ainda não possui uma conta?</h4>
-						<a href="cadastroclienteView.php">Crie grátis</a>
-					</div>
-					<div class="row text-center" style="margin-top: 20px;"> 
-						<a href="<?php echo $loginUrl; ?>">
-							<button type="button" class="btn btn-primary">Facebook</button>
-						</a>
-					</div>
-					
-					
-					
-				</form>
-			</div>
-		</div>			
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	</body>
-</html>
+							<div class="form-group">										
+								<h4>Você ainda não possui uma conta?</h4>
+								<a href="cadastroclienteView.php">Crie grátis</a>
+							</div>
+
+							<div class="form-group">										
+								<a href="<?php echo $loginUrl; ?>">
+									<button type="button" class="btn btn-primary">Facebook</button>
+								</a>
+							</div>	
+						</form>	
+					</div>															
+				</div>
+			</div>		
+		</div>	
+
+<?php include_once '../view/footerView.php'; ?>
