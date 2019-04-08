10213c186cc69139d68026bc9fa0865628212040
Eduardo Henrique <eduardo18delta@gmail.com>
2019-04-08 09:36:16 -0300

    clear

---
+++ controller/facebookloginController.php
@@ -0,0 +1,80 @@
+<?php 
+session_start();
+//unset($_SESSION['face_access_token']);
+
+# Arquivos necessários para autenticaçã via facebook
+require_once '../assets/lib/Facebook/autoload.php';
+include_once '../model/autoload.php';
+
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '2162318747411837',
+  'app_secret' => '7c3a67d23a9aa3807033f9b330046572',
+  'default_graph_version' => 'v2.9',
+  //'default_access_token' => '{access-token}', // optional
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+@$_SESSION['FBRLH_state']=$_GET['state'];
+//var_dump($helper);
+$permissions = ['email']; // Optional permissions
+
+try {
+	if(isset($_SESSION['face_access_token'])){
+		$accessToken = $_SESSION['face_access_token'];
+	}else{
+		$accessToken = $helper->getAccessToken();
+	}
+	
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	// When Graph returns an error
+	echo 'Graph returned an error: ' . $e->getMessage();
+	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+}
+
+if (! isset($accessToken)) {
+	$url_login = 'http://localhost/login/face.php';
+	$loginUrl = $helper->getLoginUrl($url_login, $permissions);
+}else{
+	$url_login = 'http://localhost/login/face.php';
+	$loginUrl = $helper->getLoginUrl($url_login, $permissions);
+	//Usuário ja autenticado
+	if(isset($_SESSION['face_access_token'])){
+		$fb->setDefaultAccessToken($_SESSION['face_access_token']);
+	}//Usuário não está autenticado
+	else{
+		$_SESSION['face_access_token'] = (string) $accessToken;
+		$oAuth2Client = $fb->getOAuth2Client();
+		$_SESSION['face_access_token'] = (string) $oAuth2Client->getLongLivedAccessToken($_SESSION['face_access_token']);
+		$fb->setDefaultAccessToken($_SESSION['face_access_token']);	
+	}
+	
+	try {
+		// Returns a `Facebook\FacebookResponse` object
+		$response = $fb->get('/me?fields=name, picture, email');
+		$user = $response->getGraphUser();
+		//var_dump($user);
+		$result_usuario = "SELECT id, nome, email FROM usuarios WHERE email='".$user['email']."' LIMIT 1";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if($resultado_usuario){
+			$row_usuario = mysqli_fetch_assoc($resultado_usuario);
+			$_SESSION['id'] = $row_usuario['id'];
+			$_SESSION['nome'] = $row_usuario['nome'];
+			$_SESSION['email'] = $row_usuario['email'];
+			header("Location: administrativo.php");			
+		}
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+	}
+}
+
+?>
+
