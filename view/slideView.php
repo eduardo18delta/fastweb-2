f0e561164be28203dfd291236604032ba0d8b571
Eduardo Henrique <eduardo18delta@gmail.com>
2019-04-09 08:47:56 -0300

    apagando css's desnecessarios e add descrição nos slides

--- view/slideView.php
+++ view/slideView.php
@@ -7,12 +7,24 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="../assets/img/slide/3.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>FastWeb</h2>
+        <h4>mais novo supermercado totalmente digital de Macapá</h4>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../assets/img/slide/2.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Aqui é mais barato</h2>
+        <h4>Aproveite as ofertas, é por tempo limitado</h4>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../assets/img/slide/1.jpg" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+        <h2>Dia da feira</h2>
+        <h4>Temos uma sessão especial de frutas e verduras</h4>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
