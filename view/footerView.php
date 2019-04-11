f46d9d99671f53ec01a8771d89e5cbcf274139f3
root <rosivan7qi@gmail.com>
2019-04-11 12:34:13 -0300

    alterado o jquery slim para o mais completo para que o modal tenha todos os efeitos

--- view/footerView.php
+++ view/footerView.php
@@ -45,7 +45,7 @@
 
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
 <script src='https://cdn.jsdelivr.net/npm/sweetalert2'></script>
