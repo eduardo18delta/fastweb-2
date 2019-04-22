267fccbff4119c40eef7f4818a45b8d7e5f5d3b0
root <rosivan7qi@gmail.com>
2019-04-22 18:32:33 -0300

    Adicionando novos campos na View cadastro de produtos

--- view/list-produtos.php
+++ view/list-produtos.php
@@ -15,68 +15,68 @@ include_once '../parts/head.php'; ?>
 <div class="container">
 
 <div class="row mt-4">
-	<div class="col-12">
-		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cadastroprodutomodal">
-			Cadastrar produto
-		</button>
+  <div class="col-12">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cadastroprodutomodal">
+      Cadastrar produto
+    </button>
 
-		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#categorias">
-			Categorias
-		</button>
-		
-	</div>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#categorias">
+      Categorias
+    </button>
+    
+  </div>
 </div>
 
 <div class="table-responsive">
-	<table class="table table-striped table-hover table-bordered mt-4">
-		<thead>
-			<tr>
+  <table class="table table-striped table-hover table-bordered mt-4">
+    <thead>
+      <tr>
         <th><input type="checkbox" name="delete"></th>
         <th>Produto:</th>
-				<th>Código:</th>
-				<th>Nome:</th>
-				<th>Valor:</th>				
-				<th>Fornecedor:</th>
-				<th>Validade</th>
-				<th>Qtd:</th>
-				<th>Marca:</th>
-				<th>Categoria</th>
-				<th>Editar:</th>
-				<th>Apagar:</th>
-			</tr>
-		</thead>
-		<tbody>
+        <th>Código:</th>
+        <th>Nome:</th>
+        <th>Valor:</th>       
+        <th>Fornecedor:</th>
+        <th>Validade</th>
+        <th>Qtd:</th>
+        <th>Marca:</th>
+        <th>Categoria</th>
+        <th>Editar:</th>
+        <th>Apagar:</th>
+      </tr>
+    </thead>
+    <tbody>
 
-		<?php foreach($lista as $produtos):?>		
-			<tr>
+    <?php foreach($lista as $produtos):?>   
+      <tr>
         <th><input type="checkbox" name="delete"></th>
         <th><img src="../assets/img/upload_produtos/<?= $produtos['img_01']?>" width="70px" height="70px"></th>
-				<td><?= $produtos['id_produto']?></td>	
-				<td><?= $produtos['nome']?></td>
-				<td><?= "R$ ".$produtos['valor']?></td> 				
-				<td><?= $produtos['fornecedor']?></td>
-				<td><?= $produtos['validade']?></td>
-				<td><?= $produtos['quantidade']?></td>
-				<td><?= $produtos['marca']?></td>
-				<td><?= $produtos['descricao']?></td>
+        <td><?= $produtos['id_produto']?></td>  
+        <td><?= $produtos['nome']?></td>
+        <td><?= "R$ ".$produtos['valor']?></td>         
+        <td><?= $produtos['fornecedor']?></td>
+        <td><?= $produtos['validade']?></td>
+        <td><?= $produtos['quantidade']?></td>
+        <td><?= $produtos['marca']?></td>
+        <td><?= $produtos['descricao']?></td>
 
-				<td>
-					<form name="deleteuser" method="post" action="../controller/deleteprodutoController.php">
-					
-					<input type="hidden" name="id" value="<?= $produtos['id_produto']?>">					
-					<input class="btn btn-danger" onclick="return confirm('Deseja realmente apagar? Produto:<?=$produtos['nome']?>');" type="submit" value="Apagar">
-					</form>
-				</td>
-				<td>				
-					<form name="updateuser" method="post" action="../view/edit-produtos.php">
-					<input type="hidden" name="id" value="<?= $produtos['id_produto']?>">						
-					<input class="btn btn-info" onclick="return confirm('Deseja realmente editar? Produto: <?= $produtos['nome']?>');" type="submit" value="Editar">
-					</form>
-				</td>
-			</tr>		
-		<?php endforeach?>
-		</tbody>
-	</table>
+        <td>
+          <form name="deleteuser" method="post" action="../controller/deleteprodutoController.php">
+          
+          <input type="hidden" name="id" value="<?= $produtos['id_produto']?>">         
+          <input class="btn btn-danger" onclick="return confirm('Deseja realmente apagar? Produto:<?=$produtos['nome']?>');" type="submit" value="Apagar">
+          </form>
+        </td>
+        <td>        
+          <form name="updateuser" method="post" action="../view/edit-produtos.php">
+          <input type="hidden" name="id" value="<?= $produtos['id_produto']?>">           
+          <input class="btn btn-info" onclick="return confirm('Deseja realmente editar? Produto: <?= $produtos['nome']?>');" type="submit" value="Editar">
+          </form>
+        </td>
+      </tr>   
+    <?php endforeach?>
+    </tbody>
+  </table>
 </div>
 
 
@@ -141,6 +141,37 @@ include_once '../parts/head.php'; ?>
           </div>
 </div>
 <div class="row">  
+          <div class="col-md-12 col-sm-12 form-group">
+          <label>Descrição</label>
+          <input class="form-control" type="text" name="descricao" required>
+          </div>
+</div>
+<div class="row"> 
+
+          <div class="col-md-3 col-sm-12 form-group">
+          <label>QTD:</label>
+          <input class="form-control" type="number" name="quantidade" required>
+          </div>
+          <div class="col-md-3 col-sm-12 form-group">
+          <label>Peso (Kg):</label>
+          <input class="form-control"  type="number" name="peso" required>
+          </div>
+          <div class="col-md-3 col-sm-12 form-group">
+          <label>UND:</label>
+          <input class="form-control"  type="number" name="peso" required>
+          </div>
+          
+          <div class="col-md-3 col-sm-12 form-group">
+            <div>MEDIDAS</div>
+            <label class="item-medida rs">R$</label>
+            <label class="item-medida kg">Kg</label>
+            <label class="item-medida und">Und</label>
+          </div>
+          <div class="col-md-1 col-sm-12 form-group">
+          <input type="hidden" name="medida" value="0" class="medida">
+          </div>        
+</div>
+<div class="row">  
           <div class="col-md-8 col-sm-12 form-group">
           <label>Fornecedor:</label>
           <input class="form-control" type="text" name="fornecedor" required>
@@ -152,13 +183,24 @@ include_once '../parts/head.php'; ?>
           </div>
 </div>
 <div class="row">  
-
-          <div class="col-md-4 col-sm-12 form-group">
-          <label>Quantidade:</label>
-          <input class="form-control" type="number" name="quantidade" required>
+          <div class="col-md-10 col-sm-12 form-group">
+          <label>Código de Barra:</label>
+          <input class="form-control" type="text" name="cod_barra" required>
           </div>
 
-          <div class="col-md-4 col-sm-12 form-group">
+          <div class="col-md-2 col-sm-12 form-group">
+          <label>Destaque:</label>
+          <input class="form-control" type="checkbox" name="destaque" value="1" required>
+          </div>
+</div>
+<div class="row">  
+          
+          <div class="col-md-3 col-sm-12 form-group">
+          <label>Desconto</label>
+          <input class="form-control"  type="number" name="desconto" required>
+          </div>
+
+          <div class="col-md-5 col-sm-12 form-group">
           <label>Marca:</label>
           <input class="form-control" type="text" name="marca" required>
           </div>
@@ -201,22 +243,22 @@ include_once '../parts/head.php'; ?>
         </button>
       </div>
       <div class="modal-body">      
-      	
-      	<table class="table table-striped table-hover table-bordered mt-4">
-      		<thead>
-      			<th>Código:</th>
-      			<th>Descrição</th>
-      		</thead>
+        
+        <table class="table table-striped table-hover table-bordered mt-4">
+          <thead>
+            <th>Código:</th>
+            <th>Descrição</th>
+          </thead>
 
-      		<tbody>
-      		<?php foreach ($listacategorias as $categorias):?>	
-			<tr>
-				<td><?=$categorias['id_categoria']?></td>
-				<td><?=$categorias['descricao']?></td>
-			</tr>			
-			<?php endforeach?>
-			</tbody>
-		</table>
+          <tbody>
+          <?php foreach ($listacategorias as $categorias):?>  
+      <tr>
+        <td><?=$categorias['id_categoria']?></td>
+        <td><?=$categorias['descricao']?></td>
+      </tr>     
+      <?php endforeach?>
+      </tbody>
+    </table>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>     
