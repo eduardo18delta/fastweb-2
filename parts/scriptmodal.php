a5932729180c529c60243052881237ea328a9c7f
root <rosivan7qi@gmail.com>
2019-04-22 18:00:14 -0300

    Alterações no script, compatível com o carrinho de compras

--- parts/scriptmodal.php
+++ parts/scriptmodal.php
@@ -1,7 +1,24 @@
 <!-- Ultimos produtos adicionados-->
 <?php 
+//============SESSÃO CARRINHO==============
+if (!isset($_SESSION)) {
+  session_start();
+} 
+     
+if(!isset($_SESSION['carrinho'])){
+   $_SESSION['carrinho'] = array();
+}
 
-          //<!--++++++++++++++++++++++++++++++++ESTANCIAMENTO+++++++++++++++++++++++++++++++++++++-->
+//REMOVER PRODUTO DO CARRINHO
+
+if(isset($_POST['rm-produto'])){
+  $id_produto = intval($_POST['form-produto-id']);
+  if(isset($_SESSION['carrinho'][$id_produto])){
+     unset($_SESSION['carrinho'][$id_produto]);
+  }
+}   
+
+//<!--++++++++++++++++++++++++++++++++ESTANCIAMENTO+++++++++++++++++++++++++++++++++++++-->
 require_once '../model/autoload.php'; $produtos_modal = new Produtos(); 
 $listar_produtos=$produtos_modal->listarDestaque();
 
@@ -20,13 +37,58 @@ $listar_produtos=$produtos_modal->listarDestaque();
      
       $(".produto'.$modal['id_produto'].'").click(function(){ 
 
+      $(".form-produto-id").val('.$modal['id_produto'].');
+      $(".modal-qtd-produto").attr("name","qtd-produto['.$modal['id_produto'].']")
       $(".background_modal").fadeIn();
       $(".produto'.$modal['id_produto'].'").addClass("addproduto'.$modal['id_produto'].'")
       $(".nome").text("'.$modal['nome'].'")
       $(".valor").text("R$ '.$modal['valor'].',00")
       $(".descricao").text("'.$modal['descricao'].'")
+      $(".desconto-icon").text("'.$modal['desconto'].'%")
+      $(".qtd-produto'.$modal['id_produto'].'").val()
+      $(".valor").addClass("valor'.$modal['id_produto'].'")
+      $(".remover-produto").addClass("remover-produto'.$modal['id_produto'].'")
+      $(".add-carrinho").addClass("add-carrinho'.$modal['id_produto'].'")
+      $(".remover-produto'.$modal['id_produto'].'").hide()
+      $(".add-carrinho'.$modal['id_produto'].'").show()
+      
 
-    
+      if ("'.$modal['medida'].'"==1){
+        $(".rs").show()
+        $(".kg").hide()
+        $(".und").hide()
+      }
+      if ("'.$modal['medida'].'"==2){
+        $(".rs").hide()
+        $(".kg").show()
+        $(".und").hide()
+      }
+      if ("'.$modal['medida'].'"==3){
+        $(".rs").show()
+        $(".kg").show()
+        $(".und").hide()
+      }
+      if ("'.$modal['medida'].'"==4){
+        $(".rs").hide()
+        $(".kg").hide()
+        $(".und").show()
+      }    
+      if ("'.$modal['medida'].'"==5){
+        $(".rs").show()
+        $(".kg").hide()
+        $(".und").show()
+      }
+      if ("'.$modal['medida'].'"==6){
+        $(".rs").hide()
+        $(".kg").show()
+        $(".und").show()
+      }
+      if ("'.$modal['medida'].'"==7){
+        $(".rs").show()
+        $(".kg").show()
+        $(".und").show()
+      }
+      
 $(".desfocar").css("filter", "blur(10px)");
 
       $(".img_produto_hover01").attr("src","../assets/img/upload_produtos/'.$modal['img_01'].'")
@@ -55,13 +117,15 @@ $(".desfocar").css("filter", "blur(10px)");
       $(".fechar_modal").click(function(){
         $(".background_modal").fadeOut();
         $(".produto'.$modal['id_produto'].'").removeClass("addproduto'.$modal['id_produto'].'")
+        $(".remover-produto").removeClass("remover-produto'.$modal['id_produto'].'")
+         $(".add-carrinho").removeClass("add-carrinho'.$modal['id_produto'].'")
         $(".desfocar").css("filter", "blur(0px)");
       })
       
       $(".comprar-produto").click(function(){
         $(".background_modal").fadeOut();
-        $(".produto'.$modal['id_produto'].'").removeClass("addproduto'.$modal['id_produto'].'")
         $(".desfocar").css("filter", "blur(0px)");
+        window.location.href = "../view/carrinhoView.php";
       })
 
       $(".add-carrinho").click(function(){
@@ -70,6 +134,32 @@ $(".desfocar").css("filter", "blur(10px)");
         $(".desfocar").css("filter", "blur(0px)");
       })
       
+
+      $(".qtd-produto'.$modal['id_produto'].'").keyup(function() {
+
+             var add_carrinho = new FormData($("#carrinho_produtos'.$modal['id_produto'].'")[0]);
+ 
+            $.ajax({
+                type: "POST",
+                //dataType: "json",
+                url: "../model/Carrinho.class.php",
+                //async: true,
+                contentType: false,
+                processData: false,
+                data: add_carrinho,
+                success: function(dados) {
+
+                 },
+                 error: function(response){
+                    alert ("erro")
+                 }
+            });
+            
+        
+            return false;
+        });
+
+
 
 });
 
