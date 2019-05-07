2e5fb81805ccb63ad21536b8a9ce1d0b6fadad5c
Eduardo Henrique <eduardo18delta@gmail.com>
2019-05-07 09:40:57 -0300

    Apagando arquivos originais de conexão

--- model/conexaoFacebook.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "drefj508";
-	$dbname = "fastweb-2";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
