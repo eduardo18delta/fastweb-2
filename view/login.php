ba7e06bd518a199dc5eb59ddc9984b6a57ef67ec
Eduardo Henrique <eduardo18delta@gmail.com>
2019-05-13 15:16:57 -0300

    Mudando nome do arquivo na chamada javascript do form de login administrativo

--- view/login.php
+++ view/login.php
@@ -27,6 +27,6 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-  <script type="text/javascript" src="../assets/js/loginadmin.js"></script>
+  <script src="../assets/js/validacoes.js"></script>
   </body>
 </html>
