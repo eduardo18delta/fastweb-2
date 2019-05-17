21fbd1d74b8a7af37880bc965d43bb51673c649e
Eduardo Henrique <eduardo18delta@gmail.com>
2019-05-16 22:57:00 -0300

    botao de cadastrar cliente grande

--- view/cadastroclienteView.php
+++ view/cadastroclienteView.php
@@ -43,7 +43,7 @@
             </div>
           
             <div class="form-group">
-                <input type="submit" name="btnCadUsuario" value="Cadastrar" class="btn btn-success">
+                <input type="submit" name="btnCadUsuario" value="Cadastrar" class="btn btn-success btn-block">
             </div>
           
             <div class="form-group">
