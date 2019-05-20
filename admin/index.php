7ff534b28b43842f42251cc90223e833105405b9
Eduardo Henrique <eduardo18delta@gmail.com>
2019-05-20 09:30:32 -0300

    Arquivo verifica se está logado ou não, redirecionando para o lugar certo: menu ou login

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,12 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/fastweb-2/view/login.php');
-	exit;
+
+session_start();
+
+if(isset($_SESSION['user'])) 
+{ 
+header("Location: ../view/menu.php");
+} 
+else 
+{ 
+header("Location: ../view/login.php"); 
+} ;
